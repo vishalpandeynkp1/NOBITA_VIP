d5e28079720769ed71d56e79a509f8c82c27e120
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-01 10:08:12 +0530

    Update couples.py

--- SONALI/plugins/tools/couples.py
+++ SONALI/plugins/tools/couples.py
@@ -14,8 +14,8 @@ from SONALI.mongo.couples_db import _get_image, get_couple
 POLICE = [
     [
         InlineKeyboardButton(
-            text="|| ˹𖤍 𝗔𝗟𝗣𝗛𝗔 𖤍˼ ||",
-            url=f"https://t.me/PURVI_SUPPORT",
+            text="|| ˹𖤍 𝐀ᴧᴙᴠɪ 𖤍˼ ||",
+            url=f"https://t.me/NOBITA_SUPPORT",
         ),
     ],
 ]
