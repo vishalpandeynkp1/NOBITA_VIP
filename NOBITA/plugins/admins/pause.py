4f787153f5b234c6fe3aa87e9e37d07b9e043d99
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-18 22:47:33 +0530

    Update pause.py

--- NOBITA/plugins/admins/pause.py
+++ NOBITA/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from SONALI import app
-from SONALI.core.call import RAUSHAN
-from SONALI.utils.database import is_music_playing, music_off
-from SONALI.utils.decorators import AdminRightsCheck
-from SONALI.utils.inline import close_markup
+from NOBITA import app
+from NOBITA.core.call import NOBI
+from NOBITA.utils.database import is_music_playing, music_off
+from NOBITA.utils.decorators import AdminRightsCheck
+from NOBITA.utils.inline import close_markup
 from config import BANNED_USERS
 
 
@@ -15,7 +15,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await RAUSHAN.pause_stream(chat_id)
+    await NOBI.pause_stream(chat_id)
     await message.reply_text(
         _["admin_2"].format(message.from_user.mention), reply_markup=close_markup(_)
     )
