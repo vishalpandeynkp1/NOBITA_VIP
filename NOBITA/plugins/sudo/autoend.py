362c207aab32f30d14f660f44f6c279fe67edc17
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 00:14:58 +0530

    Update autoend.py

--- NOBITA/plugins/sudo/autoend.py
+++ NOBITA/plugins/sudo/autoend.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from SONALI import app
-from SONALI.misc import SUDOERS
-from SONALI.utils.database import autoend_off, autoend_on
+from NOBITA import app
+from NOBITA.misc import SUDOERS
+from NOBITA.utils.database import autoend_off, autoend_on
 
 
 @app.on_message(filters.command("autoend") & SUDOERS)
