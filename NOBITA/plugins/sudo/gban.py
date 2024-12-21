a805f0ee903e807f96037202b04a9d21f4ee9321
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 00:16:35 +0530

    Update gban.py

--- NOBITA/plugins/sudo/gban.py
+++ NOBITA/plugins/sudo/gban.py
@@ -4,10 +4,10 @@ from pyrogram import filters
 from pyrogram.errors import FloodWait
 from pyrogram.types import Message
 
-from SONALI import app
-from SONALI.misc import SUDOERS
-from SONALI.utils import get_readable_time
-from SONALI.utils.database import (
+from NOBITA import app
+from NOBITA.misc import SUDOERS
+from NOBITA.utils import get_readable_time
+from NOBITA.utils.database import (
     add_banned_user,
     get_banned_count,
     get_banned_users,
@@ -15,8 +15,8 @@ from SONALI.utils.database import (
     is_banned_user,
     remove_banned_user,
 )
-from SONALI.utils.decorators.language import language
-from SONALI.utils.extraction import extract_user
+from NOBITA.utils.decorators.language import language
+from NOBITA.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
