905268607621d4108ed820bd0e9954961cce2b87
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 00:08:12 +0530

    Update seeker.py

--- NOBITA/plugins/misc/seeker.py
+++ NOBITA/plugins/misc/seeker.py
@@ -1,7 +1,7 @@
 import asyncio
 
-from SONALI.misc import db
-from SONALI.utils.database import get_active_chats, is_music_playing
+from NOBITA.misc import db
+from NOBITA.utils.database import get_active_chats, is_music_playing
 
 
 async def timer():
