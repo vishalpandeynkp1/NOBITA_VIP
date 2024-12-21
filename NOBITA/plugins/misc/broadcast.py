5108050618c30525fb893e6713a42a2dc864d1c7
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 00:07:36 +0530

    Update broadcast.py

--- NOBITA/plugins/misc/broadcast.py
+++ NOBITA/plugins/misc/broadcast.py
@@ -4,17 +4,17 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.errors import FloodWait
 
-from SONALI import app
-from SONALI.misc import SUDOERS
-from SONALI.utils.database import (
+from NOBITA import app
+from NOBITA.misc import SUDOERS
+from NOBITA.utils.database import (
     get_active_chats,
     get_authuser_names,
     get_client,
     get_served_chats,
     get_served_users,
 )
-from SONALI.utils.decorators.language import language
-from SONALI.utils.formatters import alpha_to_int
+from NOBITA.utils.decorators.language import language
+from NOBITA.utils.formatters import alpha_to_int
 from config import adminlist
 
 IS_BROADCASTING = False
@@ -117,7 +117,7 @@ async def braodcast_message(client, message, _):
     if "-assistant" in message.text:
         aw = await message.reply_text(_["broad_5"])
         text = _["broad_6"]
-        from SONALI.core.userbot import assistants
+        from NOBITA.core.userbot import assistants
 
         for num in assistants:
             sent = 0
