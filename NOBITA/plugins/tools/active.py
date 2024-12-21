438df8653d4af13dc2bf88bd25649b11be899aff
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 00:28:57 +0530

    Update active.py

--- NOBITA/plugins/tools/active.py
+++ NOBITA/plugins/tools/active.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 from unidecode import unidecode
 
-from SONALI import app
-from SONALI.misc import SUDOERS
-from SONALI.utils.database import (
+from NOBITA import app
+from NOBITA.misc import SUDOERS
+from NOBITA.utils.database import (
     get_active_chats,
     get_active_video_chats,
     remove_active_chat,
