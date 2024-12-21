456a69556451b0db13c84c7d019fcdd8d660323f
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 00:01:32 +0530

    Update start.py

--- NOBITA/plugins/bot/start.py
+++ NOBITA/plugins/bot/start.py
@@ -7,12 +7,12 @@ from youtubesearchpython.__future__ import VideosSearch
 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
 import config
-from SONALI import app
-from SONALI.misc import _boot_
-from SONALI.plugins.sudo.sudoers import sudoers_list
-from SONALI.utils.database import get_served_chats, get_served_users, get_sudoers
-from SONALI.utils import bot_sys_stats
-from SONALI.utils.database import (
+from NOBITA import app
+from NOBITA.misc import _boot_
+from NOBITA.plugins.sudo.sudoers import sudoers_list
+from NOBITA.utils.database import get_served_chats, get_served_users, get_sudoers
+from NOBITA.utils import bot_sys_stats
+from NOBITA.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -20,9 +20,9 @@ from SONALI.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from SONALI.utils.decorators.language import LanguageStart
-from SONALI.utils.formatters import get_readable_time
-from SONALI.utils.inline import help_pannel, private_panel, start_panel
+from NOBITA.utils.decorators.language import LanguageStart
+from NOBITA.utils.formatters import get_readable_time
+from NOBITA.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
