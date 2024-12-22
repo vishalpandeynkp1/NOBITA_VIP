5d139fd7e19e003cd0b1189d6ed0385761f4c005
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 09:32:34 +0530

    Update quote.py

--- NOBITA/plugins/tools/quote.py
+++ NOBITA/plugins/tools/quote.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from SONALI import app
+from NOBITA import app
 from httpx import AsyncClient, Timeout
 # -----------------------------------------------------------------
 fetch = AsyncClient(
