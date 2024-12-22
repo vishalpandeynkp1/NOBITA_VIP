e2de5837881212a8d36ac6a1868c691b376b7075
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-12-22 16:52:23 +0530

    Update fonts.py

--- NOBITA/plugins/yumi/fonts.py
+++ NOBITA/plugins/yumi/fonts.py
@@ -1,7 +1,7 @@
 from pyrogram import  filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
-from NOBITA.utils.VASU_font import Fonts
+from NOBITA.utils.NOBI_font import Fonts
 from NOBITA import app
 
 @app.on_message(filters.command(["font", "fonts"]))
