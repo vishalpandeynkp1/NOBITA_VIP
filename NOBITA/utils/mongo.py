c588acd0011a2b334468f2e565eee67097a422c0
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-12-22 15:58:26 +0530

    Update mongo.py

--- NOBITA/utils/mongo.py
+++ NOBITA/utils/mongo.py
@@ -6,7 +6,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
 from config import MONGO_DB_URI
 
 mongo = MongoCli(MONGO_DB_URI)
-db = mongo.SONALI
+db = mongo.NOBITA
 
 coupledb = db.couple
 
