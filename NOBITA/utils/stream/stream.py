8debb8a05a83134b426f4981b3bba66077526616
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-12-22 15:21:03 +0530

    Update stream.py

--- NOBITA/utils/stream/stream.py
+++ NOBITA/utils/stream/stream.py
@@ -6,20 +6,20 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from SONALI import Carbon, YouTube, app
-from SONALI.core.call import RAUSHAN
-from SONALI.misc import db
-from SONALI.utils.database import add_active_video_chat, is_active_chat
-from SONALI.utils.exceptions import AssistantErr
-from SONALI.utils.inline import (
+from NOBITA import Carbon, YouTube, app
+from NOBITA.core.call import NOBI
+from NOBITA.misc import db
+from NOBITA.utils.database import add_active_video_chat, is_active_chat
+from NOBITA.utils.exceptions import AssistantErr
+from NOBITA.utils.inline import (
     aq_markup,
     close_markup,
     stream_markup,
     telegram_markup,
 )
-from SONALI.utils.pastebin import RAUSHANBin
-from SONALI.utils.stream.queue import put_queue, put_queue_index
-from SONALI.utils.thumbnails import get_thumb
+from NOBITA.utils.pastebin import NOBIBin
+from NOBITA.utils.stream.queue import put_queue, put_queue_index
+from NOBITA.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -38,7 +38,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await RAUSHAN.force_stop_stream(chat_id)
+        await NOBITA.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -91,7 +91,7 @@ async def stream(
                         )
                     except Exception:
                         raise AssistantErr(_["play_14"])
-                await RAUSHAN.join_call(
+                await NOBITA.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -128,7 +128,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await RAUSHANBin(msg)
+            link = await NOBIBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -182,7 +182,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await RAUSHAN.join_call(
+            await NOBITA.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -242,7 +242,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await RAUSHAN.join_call(chat_id, original_chat_id, file_path, video=None)
+            await NOBITA.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -294,7 +294,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await RAUSHAN.join_call(chat_id, original_chat_id, file_path, video=status)
+            await NOBITA.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -350,7 +350,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await RAUSHAN.join_call(
+            await NOBITA.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -408,7 +408,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await RAUSHAN.join_call(
+            await NOBITA.join_call(
                 chat_id,
                 original_chat_id,
                 link,
