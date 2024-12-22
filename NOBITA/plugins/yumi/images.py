5c768a707c866dc8f12bd207605b15ec08470eea
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 10:03:51 +0530

    Update images.py

--- NOBITA/plugins/yumi/images.py
+++ NOBITA/plugins/yumi/images.py
@@ -4,7 +4,7 @@ from re import findall
 from bing_image_downloader import downloader
 from pyrogram import Client, filters
 from pyrogram.types import InputMediaPhoto, Message
-from SONALI import app
+from NOBITA import app
 
 @app.on_message(filters.command(["img", "image"], prefixes=["/", "!"]))
 async def google_img_search(client: Client, message: Message):
