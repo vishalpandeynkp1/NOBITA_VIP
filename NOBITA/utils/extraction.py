a0f26d751d0a17727925c12da60ee816f1dbf5d0
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2024-12-22 15:49:53 +0530

    Update extraction.py

--- NOBITA/utils/extraction.py
+++ NOBITA/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from SONALI import app
+from NOBITA import app
 
 
 async def extract_user(m: Message) -> User:
