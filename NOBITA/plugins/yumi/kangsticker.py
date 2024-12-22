4addf5ba1f8e549b3fc58e83a0e00ff190f72676
NOBITA PAPA <fifaworldincome@gmail.com>
2024-12-22 10:04:44 +0530

    Update kangsticker.py

--- NOBITA/plugins/yumi/kangsticker.py
+++ NOBITA/plugins/yumi/kangsticker.py
@@ -14,17 +14,17 @@ from pyrogram.errors import (
     UserIsBlocked,
 )
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from SONALI import app
+from NOBITA import app
 from config import BOT_USERNAME
-from SONALI.utils.errors import capture_err
+from NOBITA.utils.errors import capture_err
 
-from SONALI.utils.files import (
+from NOBITA.utils.files import (
     get_document_from_file_id,
     resize_file_to_sticker_size,
     upload_document,
 )
 
-from SONALI.utils.stickerset import (
+from NOBITA.utils.stickerset import (
     add_sticker_to_set,
     create_sticker,
     create_sticker_set,
