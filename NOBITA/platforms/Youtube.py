3b7f9df1e8081179a5d123a2f792caa08f8986e6
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2024-12-29 00:56:06 +0530

    Update Youtube.py

--- NOBITA/platforms/Youtube.py
+++ NOBITA/platforms/Youtube.py
@@ -26,7 +26,7 @@ async def shell_cmd(cmd):
     return out.decode("utf-8")
 
 
-cookies_file = "NOBITA/assets/cookies.txt"
+cookies_file = "assets/cookies.txt"
 
 class YouTubeAPI:
     def __init__(self):
